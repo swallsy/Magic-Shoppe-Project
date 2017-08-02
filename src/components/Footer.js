04a82171004b7b255a503f277467b9679760c721
chadwick <cekeller6121@yahoo.com>
2017-08-02 13:59:29 -0400

    basic header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../styles/Footer.css';
 
 export default class Footer extends Component {
 
   render () {
     return (
-      <div className="footer">This is the footer</div>
+      <div className="footer">Ye Olde Magic Shoppe</div>
     )
   }
 }
