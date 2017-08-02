2650f4a2a60b36316ec23855eb13c9a817277368
chadwick <cekeller6121@yahoo.com>
2017-08-02 11:58:54 -0400

    add bootstrap to product page, enabled tabs for product description and reviews

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -6,15 +6,34 @@ class ProductPage extends Component {
 
   render () {
     return (
-      <div className="mainContent">
+      <div className="pageContainer">
+      <div className="productContainer">
         <div className="imageAndDescContainer">
-        <img src={image} className="productImage" />
-        <div className="productDescriptionGroup">
-        <span className="productTitle">Product Description Title</span>
-        <span className="productDescription">Product Description</span>
+          <img src={image} className="productImage" />
+          <div className="productDescriptionGroup">
+            <span className="productTitle">Product Title</span>
+            <span className="productDescriptionPreview">Product Description Preview</span>
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
+      <div className="revsDescContainer">
+        <ul className="nav nav-tabs">
+    <li className="active"><a data-toggle="tab" href="#description">Description</a></li>
+    <li><a data-toggle="tab" href="#reviews">Reviews</a></li>
+  </ul>
+
+  <div className="tab-content">
+    <div id="description" className="tab-pane fade in active">
+      <h3>Description</h3>
+      <p>enter production description here</p>
+    </div>
+    <div id="reviews" className="tab-pane fade">
+      <h3>Reviews</h3>
+      <p>enter product reviews here</p>
+    </div>
+  </div>
+      </div>
+      </div>
     )
   }
 }
