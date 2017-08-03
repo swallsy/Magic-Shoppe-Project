4efebb060d47c6fbb96cc58f8451827868dcb782
swallsy <smwall84@gmail.com>
2017-08-03 09:41:44 -0400

    adds icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,9 @@ export default class Header extends Component {
       <div className="header">
         <div className="container">
           <nav className="navbar">
-            <a href='/'><i className="fa fa-home navbar-brand links"></i></a>
+            <a href='/' className="navbar-brand"></a>
             <div id="links_container" className="navbar-nav d-flex flex-row justify-content-center">
+
               <a href='/product' className="nav-item nav-link px-5 pb-3">Product page</a>
               <a href='/receipt' className="nav-item nav-link px-5 pb-3">Receipt</a>
               <a href='/cart' className="nav-item nav-link px-5 pb-3">Shopping cart</a>
