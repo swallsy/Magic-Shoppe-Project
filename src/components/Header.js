3205b0971046d59a19900f83d701c9961e1a9b4c
swallsy <smwall84@gmail.com>
2017-08-03 13:18:30 -0400

    adds styling to the shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default class Header extends Component {
           <nav className="navbar">
             <a href='/' className="navbar-brand"></a>
             <div id="links_container" className="navbar-nav d-flex flex-row justify-content-center">
-
+              <a href='/' className="nav-item nav-link px-5 pb-3">Home</a>
               <a href='/product' className="nav-item nav-link px-5 pb-3">Product page</a>
               <a href='/receipt' className="nav-item nav-link px-5 pb-3">Receipt</a>
               <a href='/cart' className="nav-item nav-link px-5 pb-3">Shopping cart</a>
