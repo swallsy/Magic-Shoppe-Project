5e1704241a66ee066bca3614d5b4cb9831130b0e
swallsy <smwall84@gmail.com>
2017-08-02 22:51:27 -0400

    updates to main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-
+import "../styles/header.css";
 
 export default class Header extends Component {
 
   render () {
     return (
       <div className="header">
-				<a href='/'><i className="fa fa-home"></i></a><a href='/'></a>
+        <div className="container">
+          <nav className="navbar">
+            <a href='/'><i className="fa fa-home navbar-brand links"></i></a>
+            <div id="links_container" className="navbar-nav d-flex flex-row justify-content-center">
+              <a href='/product' className="nav-item nav-link px-5 pb-3">Product page</a>
+              <a href='/receipt' className="nav-item nav-link px-5 pb-3">Receipt</a>
+              <a href='/cart' className="nav-item nav-link px-5 pb-3">Shopping cart</a>
+            </div>
+          </nav>
+        </div>
 			</div>
     )
   }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
 import "../styles/MainPage.css";
 
-class MainPage extends Component {
-  render () {
-    return (
-      <div className="home-wrapper container-fluid">
+ class MainPage extends Component   {
+   constructor(props) {
+         super(props)
+         this.state = {
+             listings: []
+         }
+     }
+
+     componentDidMount() {
+      fetch('https://guarded-retreat-23297.herokuapp.com/products')
+        .then (response => response.json())
+        .then (response => {
+            console.log(response);
+
+        let itemCards = response.map(product => {
+            return(
+                <div key={product.id} className="col-md-4">
+                  <div className="card">
+                    <img alt="card" src={product.image} className="card-img-top display-block photo" />
+                    <div className="card-block card-info">
+                      <h4 className="card-title item-name">{product.name}</h4><span>$</span><span className="price">{product.price}</span>
+                      <br/>
+                      <p className="card-text description">{product.description}</p>
+                      <a href="#" className="btn btn-primary view-item">view item</a>
+                    </div>
+                  </div>
+              </div>
+            )
+        })
+            this.setState({listings: itemCards})
+            console.log(this.state.listings);
+     })
+      .catch(function (error) {
+             console.log(error);
+      });
+    }
+
+    render() {
+    return(
+      <div className="home-wrapper container">
         <div id="splash-area">
+          <h1 id="title"><span>Ye Olde</span><br />Magic Shoppe</h1>
           <div id="bgvid">
             <video loop muted autoPlay className="fullscreen-bg__video">
-                <source src="../matcha/Mp4/Matcha.mp4" type="video/mp4" />
+              <source src="../matcha/Mp4/Matcha.mp4" type="video/mp4" />
             </video>
           </div>
         </div>
-				<br/>
-				<p><a href='/product'>Product page</a></p>
-				<p><a href='/receipt'>Receipt</a></p>
-				<p><a href='/cart'>Shopping cart</a></p>
-			</div>
-    )
+        <div id="main-wrapper">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="row card-group" id="cards-area">
+                {this.state.listings}
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+      )
+    }
   }
-
-}
 
 export default MainPage;
