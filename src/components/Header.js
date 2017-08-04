0278cd7807479eddedb3091d07fe771aed2198b3
swallsy <smwall84@gmail.com>
2017-08-04 14:08:09 -0400

    adds styling to the product page, merges nav into the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,15 +9,11 @@ export default class Header extends Component {
     return (
       <div className="header">
         <div className="container">
-					<NavBar />
           <nav className="navbar">
             <a href='/' className="navbar-brand"></a>
             <div id="links_container" className="navbar-nav d-flex flex-row justify-content-center">
-              <a href='/' className="nav-item nav-link px-5 pb-3">Home</a>
-              <a href='/product' className="nav-item nav-link px-5 pb-3">Product page</a>
-              <a href='/receipt' className="nav-item nav-link px-5 pb-3">Receipt</a>
-              <a href='/cart' className="nav-item nav-link px-5 pb-3">Shopping cart</a>
-            </div>
+              <NavBar />
+            </div>        
           </nav>
         </div>
 			</div>
